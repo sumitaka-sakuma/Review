9a7f9fa49c4ab679bbc17e8e273a5badff83c04e
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-02-07 12:04:56 +0900

    implement

---
+++ Review/src/service/MemberServiceImpl.java
@@ -0,0 +1,30 @@
+package service;
+
+import java.util.ArrayList;
+
+import Entity.Member;
+
+public class MemberServiceImpl implements MemberService {
+
+	@Override
+	public String greet(int i) {
+		
+		String[] greetings = {"Good morning", "Hello", "Good evening"};
+		
+		return greetings[i];
+	}
+
+	@Override
+	public ArrayList<Member> getAll() {
+		
+		ArrayList<Member> memberList = new ArrayList<>();
+		Member mem1 = new Member(1, "Tom", "tom@gmail.com");
+		Member mem2 = new Member(1, "Mary", "mary@gmail.com");
+		
+		memberList.add(mem1);
+		memberList.add(mem2);
+		
+		return memberList;
+	}
+
+}
